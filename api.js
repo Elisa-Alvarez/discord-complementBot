7a781e5e3b12e7a2124fcc9e48d5f40ff87a70eb
Elisa ALvarez <elisaalvarez211@gmail.com>
2022-01-26 15:22:14 -0800

    Need to add original sheet and seperate based upon that

---
+++ api.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+fs = require('fs')
+const { google } = require("googleapis");
+const spreadsheetId = process.env.SPREADSHEETID
+
+ async function getSheetData (){
+
+const auth =  new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets.readonly",
+});
+
+  // Create client instance for auth
+const gclient =  await auth.getClient();
+
+  // Instance of Google Sheets API
+const googleSheets = google.sheets({ version: "v4", auth: gclient });
+
+const getRows =  await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "COMPLIMENT!A:D",
+  });
+
+  const defaultArray = getRows.data.values
+  let currentJson ={}
+  let jsonArr =[]  
+  defaultArray.forEach(index =>{
+    currentJson = {...currentJson,id:index[0], command:index[1], response:index[2]} 
+    jsonArr.push(currentJson)
+    
+  })
+  jsonArr.shift()
+  
+  if(fs.existsSync('./data.json')){
+    fs.unlinkSync('./data.json')
+    fs.writeFile('./data.json',JSON.stringify(jsonArr), (err)=>{
+      if (err) throw err
+    })
+  }else{
+    fs.writeFile('./data.json',JSON.stringify(jsonArr), (err)=>{
+      if (err) throw err
+    })
+  }
+
+}
+
+const d = new Date()
+const hours = d.getUTCHours()
+const min =  d.getUTCMinutes()
+
+if(hours && min === 0){
+  getSheetData()
+}
--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 require("dotenv").config();
-const Discord = require('discord.js')
-const client = new Discord.Client();
+const data = require('./data.json')
+const { Client, Intents } = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const prefix = ".="
 
+
+console.log(data)
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`)
+  console.log(`Logged in as ${client.user.tag}!`)
+
+})
+
+
+client.on("messageCreate", msg => {
+    if (!msg.content.startsWith(prefix)|| msg.author.bot) return
+
+    const args = msg.content.split(prefix.length).splice(/ +/);
+    const userInput = args.shift().toLowerCase()
+
+    switch (userInput){
+
+    //  case data[0].command:
+        // let res = data[0].response.replace("{user}", `${msg.author}`);
+        // res = res.replaceAll(/["|"]/g, ``)
+        // msg.channel.send(res);
+        // break
+      //Move the replace funtion to utils file and import as a varible
+     case '/burn':
+     msg.channel.send("You Suck Frogs");
+     
+     default:
+     break
+     }
+
+    
   })
-  
-  client.on("message", msg => {
-    if (msg.content === "ping") {
-      msg.reply("pong");
-    }
-  })
-  
 client.login(process.env.CLIENTID)
