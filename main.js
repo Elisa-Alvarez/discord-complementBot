0afef182e4a25864e912b461ebbceabf779d4fa4
Elisa ALvarez <elisaalvarez211@gmail.com>
2022-02-04 18:41:28 -0800

    Flirting is restricted to one channel and need to add channel link

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ const api = require('./api')
 const schedule = require('node-schedule')
 const { Client, Intents } = require('discord.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const prefix = ".="
+const prefix = process.env.PREFIX
 const {priaseCommand,flirtCommand,roastCommand} = require('./util')
 const priase = priaseCommand()
 const flirt = flirtCommand()
 const roast = roastCommand()
-console.log(roast)
+
 client.on("ready", () => {
   if(fs.existsSync('./data.json')){
+    console.log(Date.UTC)
   }else{
     async () =>await api
   }
@@ -19,21 +20,18 @@ client.on("ready", () => {
 
 client.on("messageCreate", msg => {
     if (!msg.content.startsWith(prefix)|| msg.author.bot) return
+    const nsfwChannel = 'art'
     const args = msg.content.split(prefix.length).splice(/ +/);
     const userInput = args.shift().toLowerCase()
     let rn = Math.floor(Math.random()*priase.length)
     let fn=Math.floor(Math.random()*flirt.length)
     let n=Math.floor(Math.random()*roast.length)
+ 
     switch (userInput){
      case priase[rn].command:
         let res = priase[rn].response.replace("{user}", `${msg.author}`);
         res = res.replaceAll(/["|"]/g, ``)
         msg.channel.send(res);
-        break
-     case flirt[fn].command:
-      let fres = flirt[fn].response.replace("{user}", `${msg.author}`);
-      fres = fres.replaceAll(/["|"]/g, ``)
-      msg.channel.send(fres);
         break
      case roast[n].command:
       let burnres = roast[n].response.replace("{user}", `${msg.author}`);
@@ -43,6 +41,14 @@ client.on("messageCreate", msg => {
      default:
         break
      }
+     if( flirt[fn].command && msg.channel.name.startsWith(nsfwChannel)){
+      let fres = flirt[fn].response.replace("{user}", `${msg.author}`);
+      fres = fres.replaceAll(/["|"]/g, ``)
+       return msg.channel.send(fres);
+     }
+    else{
+        return  msg.channel.send(`Sorry my boss is watching, if you want to be flirty let's head to the art channel ${msg.author}`)
+    }
 
     
   })
